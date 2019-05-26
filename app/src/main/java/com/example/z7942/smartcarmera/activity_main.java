2061ab04d813f9dbfd97492686f992681c871b63
hy1152 <wogjs1152@gmail.com>
2019-05-26 21:15:44 +0900

    google cloud vision api test

--- app/src/main/java/com/example/z7942/smartcarmera/activity_main.java
+++ app/src/main/java/com/example/z7942/smartcarmera/activity_main.java
@@ -3,6 +3,7 @@ package com.example.z7942.smartcarmera;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,38 +17,6 @@ public class activity_main extends AppCompatActivity {
 
     Button PicSearch, NameSearch, Setting;
 
-
-    public static void main(String[] args) {
-        String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
-        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
-        try {
-            String text = URLEncoder.encode("그린팩토리", "UTF-8");
-            String apiURL = "https://openapi.naver.com/v1/search/shop.json="+ text; // json 결과
-            //String apiURL = "https://openapi.naver.com/v1/search/shop.xml="+ text; // xml 결과
-            URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-Naver-Client-Id", clientId);
-            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
-            }
-            br.close();
-            System.out.println(response.toString());
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedlnstanceState){
         super.onCreate(savedlnstanceState);
@@ -60,8 +29,10 @@ public class activity_main extends AppCompatActivity {
         PicSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), activity_camera.class);
+                Intent intent = new Intent(getApplicationContext(), test.class);
                 startActivity(intent);
+                Log.d("TEST1","HERE");
+                Log.d("TEST2", String.valueOf(intent));
             }
         });
 
